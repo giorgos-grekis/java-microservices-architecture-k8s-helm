e19cd5d2a498d13fb81d908712fa649784430629
giorgos-grekis <georgrekis.github@gmail.com>
2026-04-05 15:40:14 +0300

    feat: add open apo docs to accountsContoller @Tag, @Operation, @ApiResponse

--- accounts/src/main/java/com/cisu/breaking/bank/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/cisu/breaking/bank/accounts/controller/AccountsController.java
@@ -4,6 +4,10 @@ import com.cisu.breaking.bank.accounts.constans.AccountsConstants;
 import com.cisu.breaking.bank.accounts.dto.CustomerDto;
 import com.cisu.breaking.bank.accounts.dto.ResponseDto;
 import com.cisu.breaking.bank.accounts.service.IAccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,6 +16,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "Accounts",
+        description = "CRUD REST APIs for Accounts "
+)
 
 @RestController
 @RequestMapping(path = "/api/v1", produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -21,6 +29,14 @@ public class AccountsController {
 
    private IAccountsService iAccountsService;
 
+   @Operation(
+           summary = "Create Account",
+           description = "API to create a new customer and account"
+   )
+   @ApiResponse(
+           responseCode = "201",
+           description = "HTTP Status CREATED"
+   )
    @PostMapping("/create")
    public ResponseEntity<ResponseDto> createAccount(
            @Valid
@@ -35,6 +51,14 @@ public class AccountsController {
                         AccountsConstants.MESSAGE_201));
    }
 
+    @Operation(
+            summary = "Get Account Details",
+            description = "fetch account details with mobile number"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status OK"
+    )
    @GetMapping("/fetch")
    public ResponseEntity<CustomerDto> fetchAccountDetails(
            @Valid @RequestParam String mobileNumber
@@ -44,6 +68,20 @@ public class AccountsController {
        return ResponseEntity.status(HttpStatus.OK).body(customerDto);
    }
 
+    @Operation(
+            summary = "Update Account Details",
+            description = "update account and customer based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    })
     @PutMapping("/update")
     public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto) {
         boolean isUpdated = iAccountsService.updateAccount(customerDto);
@@ -58,6 +96,21 @@ public class AccountsController {
         }
     }
 
+
+    @Operation(
+            summary = "Delete Account & Customer Details",
+            description = "delete account and customer based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    })
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDto> deleteAccountDetails(
 //            @PhoneNumber
